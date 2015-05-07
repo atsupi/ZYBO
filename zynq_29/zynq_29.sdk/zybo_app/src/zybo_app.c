f0fae37bd0b5a628af2fdc8c2574b4007b29a763
atsupi <atsupi@gmail.com>
2015-05-08 04:13:41 +0900

    SDK Source Code initiation

---
+++ zynq_29/zynq_29.sdk/zybo_app/src/zybo_app.c
@@ -0,0 +1,14 @@
+//
+// zybo_app.c
+//
+// Author: atsupi
+// Date: 2015/05/07
+
+#include <xil_printf.h>
+#include "xparameters.h"
+
+int main()
+{
+    xil_printf("Hello World\n\r");
+    return 0;
+}
